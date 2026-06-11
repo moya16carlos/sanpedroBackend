dce9751cbba496e6e120610cd93716002c40e3d6
Carlos Moya Riquelme <carlosmoyariquelme@192.168.1.40>
2026-06-10 21:05:37 -0400

    Configurar STATIC_ROOT para Docker

--- config/settings.py
+++ config/settings.py
@@ -190,3 +190,14 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 SOCIALACCOUNT_ADAPTER = 'core.adapters.GoogleAccessAdapter'
+
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
